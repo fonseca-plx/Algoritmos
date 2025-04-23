038f8d021d77321293dda731db0be2ce9e22b89b
Pedro Fonseca <153639320+fonseca-plx@users.noreply.github.com>
2025-04-23 23:29:03 +0000

    Update listas/lista02/10.cpp

--- listas/lista02/10.cpp
+++ listas/lista02/10.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 #include <cmath>
 
-double raiz_q_rec(double rc, double r) {
+double raiz_q_rec(double rc, double r, double n) {
     double dif = std::abs(rc - r);
-    if (dif < 0.0001) {
+    if (dif < 0.000001) {
         return r;
     }
     rc = r;
-    return raiz_q_rec(rc, (rc + n/rc) / 2.0);
+    return raiz_q_rec(rc, (rc + n/rc) / 2.0, n);
 }
 
 double raiz_q(double n) {
     double rc = n/2.0;
     double r = (rc + n/rc) / 2.0;
-    return raiz_q_rec(rc, r);
+    return raiz_q_rec(rc, r, n);
 }
 
 int main() {
