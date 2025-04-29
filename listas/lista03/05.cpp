edeca1bb920f5e82efa12da047b7ebd0f01f3aaa
Pedro Fonseca <153639320+fonseca-plx@users.noreply.github.com>
2025-04-29 14:59:46 +0000

    Solving questions (05 e 06)

---
+++ listas/lista03/05.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+bool primo(int num) {
+    if (num <= 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+
+    int limite = static_cast<int>(sqrt(num));
+    for (int i = 3; i <= limite; i += 2) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int conta_primos(int a[], int n, int primos[]) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (primo(a[i])) {
+            primos[count] = a[i];
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    int a[n], primos[n]; // n corresponde a maior posição possível para primos
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    int resultado = conta_primos(a, n, primos);
+    std::cout << "\nQuantidade de n primos: " << resultado << std::endl;
+    if (resultado > 0) {
+        std::cout << primos[0];
+        for (int i = 1; i < resultado; i++) {
+            std::cout << ", " << primos[i];
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
--- listas/lista03/06.cpp
+++ listas/lista03/06.cpp
@@ -1,1 +1,32 @@
-// resolve com pitágoras a² = b² + c²
+// resolve com pitágoras a² = b² + c² (hypot)
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+struct coordenada {
+    double x, y;
+};
+
+double menor_distancia(coordenada pontos[], int n) {
+    double d;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; i++) {
+            double cordx = pontos[i].x - pontos[j].x;
+            double cordy = pontos[i].y - pontos[j].y;
+            if ()
+        }
+    }
+    return d;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    coordenada pontos[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> pontos[i].x >> pontos[i].y;
+    }
+    double distancia = menor_distancia(pontos, n);
+    std::cout << std::fixed << std::setprecision(4) << distancia << std::endl;
+    return 0;
+}
